d8efc434e4b554f6ab829a75f75ecb2ff89c62e6
giften <giftennthankachan@gmail.com>
2021-11-14 22:39:48 +0530

    upadate

---
+++ assets/js/script.js
@@ -0,0 +1,111 @@
+
+
+// $('#image-gallery').lightSlider({
+//     gallery:true,
+//     item:1,
+//     thumbItem:9,
+//     slideMargin: 0,
+//     speed:500,
+//     auto:true,
+//     loop:true,
+//     onSliderLoad: function() {
+//         $('#image-gallery').removeClass('cS-hidden');
+//     }  
+// });
+
+// Navigation active state on scroll
+var nav_sections = $('section');
+var main_nav = $('.nav-menu, .mobile-nav');
+
+$(window).on('scroll', function() {
+  var cur_pos = $(this).scrollTop() + 200;
+
+  nav_sections.each(function() {
+    var top = $(this).offset().top,
+      bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      if (cur_pos <= bottom) {
+        main_nav.find('li').removeClass('active');
+      }
+      main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+    }
+    if (cur_pos < 300) {
+      $(".nav-menu ul:first li:first, .mobile-menu ul:first li:first").addClass('active');
+    }
+  });
+});
+
+// Mobile Navigation
+if ($('.nav-menu').length) {
+  var $mobile_nav = $('.nav-menu').clone().prop({
+    class: 'mobile-nav d-lg-none'
+  });
+  $('body').append($mobile_nav);
+  $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+  $('body').append('<div class="mobile-nav-overly"></div>');
+
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+    $('.mobile-nav-overly').toggle();
+  });
+
+  $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+    e.preventDefault();
+    $(this).next().slideToggle(300);
+    $(this).parent().toggleClass('active');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav, .mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-overly').fadeOut();
+      }
+    }
+  });
+} else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+  $(".mobile-nav, .mobile-nav-toggle").hide();
+}
+
+// Toggle .header-scrolled class to #header when page is scrolled
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  } else {
+    $('#header').removeClass('header-scrolled');
+  }
+});
+
+if ($(window).scrollTop() > 100) {
+  $('#header').addClass('header-scrolled');
+}
+
+
+   
+//quantity
+      function wcqib_refresh_quantity_increments() {
+        jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
+            var c = jQuery(b);
+            c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+        })
+    }
+    String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+        var a = this,
+            b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+        return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+    }), jQuery(document).ready(function() {
+        wcqib_refresh_quantity_increments()
+    }), jQuery(document).on("updated_wc_div", function() {
+        wcqib_refresh_quantity_increments()
+    }), jQuery(document).on("click", ".plus, .minus", function() {
+        var a = jQuery(this).closest(".quantity").find(".qty"),
+            b = parseFloat(a.val()),
+            c = parseFloat(a.attr("max")),
+            d = parseFloat(a.attr("min")),
+            e = a.attr("step");
+        b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+    });
